bdddbc23fb673d0dbdc14cacfaab62b07af79c06
johnpaulcas <casjp.app@gmail.com>
2024-01-01 11:06:00 +0800

    :sparkles: Add premint interfaces

--- src/interfaces/IBaseERC721Mint.sol
+++ src/interfaces/IBaseERC721Mint.sol
@@ -17,4 +17,42 @@ interface IBaseERC721Mint {
      * @notice The receiver of the tokens will be the `msg.sender`.
      */
     function mint(uint256 _numberOfTokens) external payable;
+
+    /**
+     * @dev Mint a specified number of tokens to the caller.
+     * @param _numberOfTokens The number of tokens to mint.
+     * @param _merkleProof The merkle proof for the caller's address.
+     * @notice The receiver of the tokens will be the `msg.sender`.
+     */
+    function premint(
+        uint256 _numberOfTokens,
+        bytes32[] calldata _merkleProof
+    ) external payable;
+
+    /**
+     * @dev Checks if a user is qualified for the premint phase by verifying their merkle proof.
+     * @param _merkleProof The merkle proof for the user's address.
+     * @return A boolean indicating whether the user is qualified for the premint phase.
+     */
+    function isQualifiedForPremint(
+        bytes32[] calldata _merkleProof
+    ) external view returns (bool);
+
+    /**
+     * @dev Sets the pre-mint merkle root.
+     * @param _merkleRoot The merkle root value to be set.
+     * Requirements:
+     * - The function can only be accessed by the caller who has `MANAGER_ROLE` access.
+     */
+    function setPremintMerkleRoot(bytes32 _merkleRoot) external;
+
+    /**
+     * @dev Sets the maximum number of tokens that can be minted per address during the premint phase.
+     * @param _premintPerAddressLimit The maximum number of tokens that can be minted per address during the premint phase.
+     * Requirements:
+     * - The function can only be accessed by the caller who has `MANAGER_ROLE` access.
+     */
+    function setPremintPerAddressLimit(
+        uint256 _premintPerAddressLimit
+    ) external;
 }
